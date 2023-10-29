28911767fbf43da13f08656a37e9ecde1581f8f3
sshinde4 <sshinde4@ncsu.edu>
2023-10-29 01:44:16 -0400

    Allow for multiple login attempts

--- src/grader.py
+++ src/grader.py
@@ -3,19 +3,24 @@
 Usage: python3 grader.py
 """
 
-__author__ = 'Siddarth Shinde'
-__version__ = '1.0.0'
-__email__ = 'sshinde4@ncsu.edu'
+__author__ = "Siddarth Shinde"
+__version__ = "1.0.0"
+__email__ = "sshinde4@ncsu.edu"
 
 import csv
 import piazza_api
-from piazza_api import Piazza
 
-def main():  
-    p = Piazza()
-    
+
+def main():
+    p = piazza_api.Piazza()
+
     # Prompt for email and password
-    p.user_login()
+    while True:
+        try:
+            p.user_login()
+            break
+        except piazza_api.exceptions.AuthenticationError as error:
+            print("Email or password incorrect")
 
     # Note: Replace with class code every semester (whatever comes after "class/" in the url)
     csc110 = p.network("llbcl58pjkd113")
@@ -35,41 +40,47 @@ def main():
     student_names_map = {}
 
     init_scores(csc110, scores, student_names_map),
-    grade(post, scores) # should update scores
+    grade(post, scores)  # should update scores
     scores_to_csv(scores, student_names_map)
+
 
 # Initializes all maps (all scores to zero and map user ids to users' names)
 def init_scores(course, user_score_map, user_to_name_map):
     global_users = course.get_all_users()
     for user in global_users:
         # if user is not staff
-        if not user.get('admin'):
+        if not user.get("admin"):
             # Set everyone's score to zero
-            user_score_map[user.get('id')] = 0
+            user_score_map[user.get("id")] = 0
             # Map user's id to their name
-            user_to_name_map[user.get('id')] = user.get('name')
+            user_to_name_map[user.get("id")] = user.get("name")
+
 
 # Grades post
 def grade(post, user_score_map):
     # Search change_log for user ids that made changes to the thread
-    for change in post.get('change_log'):
-        curr_id = change.get('uid')
+    for change in post.get("change_log"):
+        curr_id = change.get("uid")
         if curr_id is not None:
             curr_score = user_score_map.get(curr_id)
             # If no posts made, set score to 70, if one already made, set to 100
             user_score_map[curr_id] = 70 if curr_score == 0 else 100
 
+
 # Outputs scores to a csv with headers: Name | Score
 def scores_to_csv(user_score_map, user_to_name_map):
-    scores_list=[]
+    scores_list = []
     for uid in user_score_map:
-        scores_list.append({'Name':user_to_name_map.get(uid), 'Score': user_score_map.get(uid)})
+        scores_list.append(
+            {"Name": user_to_name_map.get(uid), "Score": user_score_map.get(uid)}
+        )
 
     # Output scores csv
-    fields = ['Name', 'Score']
-    with open('scores.csv', 'w') as csvf:
-        writer = csv.DictWriter(csvf, fieldnames = fields)
+    fields = ["Name", "Score"]
+    with open("scores.csv", "w") as csvf:
+        writer = csv.DictWriter(csvf, fieldnames=fields)
         writer.writeheader()
         writer.writerows(scores_list)
+
 
 main()
